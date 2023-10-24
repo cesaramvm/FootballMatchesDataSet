86e73eef3cda1a9c78f739c9b5fa1ae7e20336da
César Valdés <cvaldesmar@gmail.com>
2023-10-24 10:40:33 +0200

    all new with fixed marketValues

--- Main.py
+++ Main.py
@@ -13,20 +13,19 @@ def main(divisiones, temporadas, loadFromFile):
     for division in divisiones:
         print("****  PROCESANDO DIVISIÓN %s ****" % division)
         for temporada in temporadas:
-            if temporada == "2022-23":
-                print(Fore.RED + "****  2022 2023 cuidadin ****"+Fore.RESET)
             print("****  PROCESANDO TEMPORADA %s ****" % temporada)
             seasonData = DatosTemporada(division, temporada, loadFromFile)
             #seasonData.printSeasonResults()
             seasonData.printSeasonWinner()
             ut.ADD_SEASON_INFO(division, temporada, seasonData)
-    ut.SAVE_ALL_SEASONS(ut.SAVE_SEASONS_PATH)
+            ut.SAVE_MARKET_VALUES(ut.SAVE_MARKET_PATH)
 
-loadFromFile = True
-temporadas = ut.SHORT_TEMPORADAS
+loadFromFile = False
+temporadas = ut.ALL_TEMPORADAS
 divisiones = [2,1]
 ut.LOAD_MARKET_VALUES(ut.SAVE_MARKET_PATH)
 main(divisiones, temporadas, loadFromFile)
 ut.SAVE_MARKET_VALUES(ut.SAVE_MARKET_PATH)
-
+ut.SAVE_ALL_SEASONS(ut.SAVE_SEASONS_PATH)
 mh.SAVE_NOT_CORRECTED_NAMES()
+mh.SAVE_NOT_IN_MARKET_NAMES()
--- MarketHelper.py
+++ MarketHelper.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 
 firstMarketDate = datetime.now().date()
 notInCorrectionTeams = set()
+notInMarketTeams = set()
 
 MARKET_INFO = {}
 
@@ -23,22 +24,22 @@ def scrapeMarketDatesToDictionary(division):
             return option_values
     raise Exception("No scraping Market Dates Found")
 
-def scrapeMarketValues(division, realMarketDate):
+def scrapeMarketTeamValues(division, realMarketDate):
     print(Fore.MAGENTA + f"Scraping {division}ª Market Values ON {realMarketDate}"+Fore.RESET)
     teamsValues = {}
     marketUrl = ut.MARKET_URL[division] % realMarketDate.strftime("%Y-%m-%d")
-    req = requests.get(marketUrl, headers={'User-Agent': 'Custom'})
+    req = requests.get(marketUrl, headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.999 Safari/537.36'})
 
     localMarketNodes = BeautifulSoup(req.text, "html.parser").find_all('td', {'class': 'hauptlink no-border-links'})
     for localNode in localMarketNodes:
         teamName = localNode.find("a")['title']
-        teamValue = localNode.find_next_siblings()[1].find("a").string
-        if(teamValue == "-"):
+        teamValueText = localNode.find_next_siblings()[1].find("a").string
+        if(teamValueText == "-"):
             teamValue = -1
         else:
-            #TODO
-            #Aqui justo a veces te entra algo como  "768,60 mill. €" o "1,06 mil mill. €"
-            teamValue = float(teamValue.split(" ")[0].replace(",","."))
+            teamValue = float(teamValueText.split(" ")[0].replace(",","."))
+            if "mil mill." in teamValueText:
+                teamValue *= 1000
         teamsValues[teamName] = teamValue
     return teamsValues
 
@@ -46,25 +47,38 @@ def getTeamsMarketValue(division, matchDate, localName, visitanteName):
     return [getMarketValue(division, localName, matchDate),getMarketValue(division, visitanteName, matchDate)]
 
 def getMarketValue(division, equipoName, matchDate):
-    matchDate = datetime.strptime(matchDate, '%Y-%m-%d').date()
     ut.CHECK_KEY_EXISTANCE(division, MARKET_INFO, lambda: scrapeMarketDatesToDictionary(division))
-    if equipoName not in ut.MARKET_NAMES_CORRECTION:
-        notInCorrectionTeams.add(equipoName)
-        return -1
+    matchDate = datetime.strptime(matchDate, '%Y-%m-%d').date()
     if matchDate < firstMarketDate:
         return -1
-        
-    equipoName = ut.MARKET_NAMES_CORRECTION[equipoName]
+    if equipoName not in ut.MARKET_NAMES_CORRECTION:
+        notInCorrectionTeams.add(equipoName)
+    else:
+        equipoName = ut.MARKET_NAMES_CORRECTION[equipoName]
     realMarketDate = min(MARKET_INFO[division], key=lambda date: abs((date - matchDate) if ((date - matchDate).days)<=0 else timedelta(days=9999)))
     if not MARKET_INFO[division][realMarketDate]:
-        MARKET_INFO[division][realMarketDate] = scrapeMarketValues(division, realMarketDate)
-        SAVE_MARKET_VALUES(ut.SAVE_MARKET_PATH)
+        MARKET_INFO[division][realMarketDate] = scrapeMarketTeamValues(division, realMarketDate)
     if equipoName not in MARKET_INFO[division][realMarketDate]:
-        return -1
-        found_keys = [key for key in MARKET_INFO[realMarketDate][division].keys() if equipoName in key]
-        print(f"equipoName:{equipoName} foundkeys: {found_keys}")
-        if found_keys:
-            return MARKET_INFO[realMarketDate][division][found_keys[0]]
+        backupDivision = 1 if division==2 else 2
+        ut.CHECK_KEY_EXISTANCE(backupDivision, MARKET_INFO, lambda: scrapeMarketDatesToDictionary(backupDivision))        
+        if not MARKET_INFO[backupDivision][realMarketDate]:
+            MARKET_INFO[backupDivision][realMarketDate] = scrapeMarketTeamValues(backupDivision, realMarketDate)
+        if equipoName in MARKET_INFO[backupDivision][realMarketDate]:
+            value = getMarketValue(backupDivision, equipoName, str(matchDate))
+            #print(f"equipoName:{equipoName} encontrado en otra div: with value {value}")
+            return value
+        found_keys = [key for key in MARKET_INFO[division][realMarketDate].keys() if equipoName in key]
+        found_keys2 = [key for key in MARKET_INFO[backupDivision][realMarketDate].keys() if equipoName in key]
+        
+        if not found_keys and not found_keys2:
+            notInMarketTeams.add(equipoName)
+            return -1
+        else:
+            print(f"equipoName:{equipoName} foundkeys: {found_keys} in div: {division}")
+            print(f"equipoName:{equipoName} foundkeys: {found_keys2} in div: {backupDivision}")
+        backupEquipoName = equipoName
+        equipoName = found_keys[0] if len(found_keys)>0 else found_keys2[0]
+        print(f"oldequipoName:{backupEquipoName} - new equipoName:{equipoName} ")
     return MARKET_INFO[division][realMarketDate][equipoName]
 
 def printMarketValues():
@@ -95,10 +109,11 @@ def SET_FIRST_MARKET_DATE(collection):
     if earliest_date < firstMarketDate:
         firstMarketDate = earliest_date
 
-    print(firstMarketDate)
-
-
-
 def SAVE_NOT_CORRECTED_NAMES():
-    with open('marketFails.txt', 'w', encoding='utf-8') as f:
+    with open('uncorrectedMarketNames.txt', 'w', encoding='utf-8') as f:
+        f.write(f"{notInCorrectionTeams}")
+
+
+def SAVE_NOT_IN_MARKET_NAMES():
+    with open('notInMarketNames.txt', 'w', encoding='utf-8') as f:
         f.write(f"{notInCorrectionTeams}")
